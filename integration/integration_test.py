39daf23f93dc828470cb885b6407ac2d9cc4972c
Hari Prasath Muthurathinam <hmuthurathinam0672@conestogac.on.ca>
2024-06-08 02:34:25 -0400

    pytest

--- integration/integration_test.py
+++ integration/integration_test.py
@@ -2,8 +2,8 @@
 import totalAndMean
 def test_total():
     # Test adding 5 numbers
-    assert totalAndMean.totalofnumbers(1,1,1,1,1) == 5
+    assert totalAndMean.totalofnumbers(1, 1, 1, 1, 1) == 5
 
 def test_mean():
     # Test mean of 5 numbers
-    assert totalAndMean.mean(totalAndMean.totalofnumbers(1,1,1,1,1),5) == 2
+    assert totalAndMean.mean(totalAndMean.totalofnumbers(1, 1, 1, 1, 1), 5) == 2
